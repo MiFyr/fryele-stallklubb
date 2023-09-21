f34d67b33ee0fa4c7a65cc76d8e4fc55d2fac6f5
Milla Fyrman <millafyrman@gmail.com>
2023-09-21 16:56:11 +0200

    added contact form

---
+++ script.js
@@ -0,0 +1,7 @@
+//page
+const kForm = document.getElementById("k-form");
+const formBtn = document.getElementById("form-btn");
+
+formBtn.addEventListener("click", function () {
+  console.log(kForm.value);
+});
